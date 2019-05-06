2428b7a8333d35f4e964f9c43026d81c9e937d2c
JustinRobertKing <kingj4@spu.edu>
2019-05-06 16:10:59 -0700

    stubbed out bounties controller

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ let express = require('express')
 
 lep app = express()
 
+// Controllers
+app.use('/v1/bounties', require('./controllers/v1/bounties'))
+
 app.get('*', (req, res) => {
-	res.send('butts')	
+	res.status(404).send('Not Found')	
 })
 
 app.listen(process.env.PORT || 3000)
